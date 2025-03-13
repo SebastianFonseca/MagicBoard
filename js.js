0878a61ed3a620af19835c679c499ba19b38ebb3
Sebastian Fonseca <sebastianfonck@hotmail.com>
2025-03-12 21:50:01 -0500

    Fixing board issues, geting some styling

--- js.js
+++ js.js
@@ -1,14 +1,33 @@
 const board = document.getElementById("board");
-(function createBoard(numberOfSquares = 16) {
+const boardAndMenu = document.getElementById("boardAndMenu");
+const boardContainer = document.querySelector("#boardContainer");
+const range = document.querySelector("#range");
+const rangeValueLabel = document.querySelector("#rangeValue");
+rangeValueLabel.textContent = range.value;
+createBoard();
+
+range.addEventListener("change", () => {
+  rangeValueLabel.textContent = range.value;
+  boardContainer.removeChild(board);
+  createBoard(range.value);
+});
+
+function createBoard(numberOfSquares = 16) {
+  let size =
+    Math.min(
+      boardAndMenu.getBoundingClientRect().width,
+      boardAndMenu.getBoundingClientRect().height
+    ) * 0.6;
+  board.style.cssText += `width:${size}px; height:${size}px;`;
+  const squareSize = size / numberOfSquares;
   for (let i = 1; i <= numberOfSquares * numberOfSquares; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("squares");
-    newDiv.style.width = `${(board.offsetWidth * 0.7) / numberOfSquares - 2}px`;
-    newDiv.style.height = `${board.offsetHeight / numberOfSquares - 2}px`;
+    newDiv.style.cssText += `width:${squareSize}px; height:${squareSize}px;`;
     newDiv.addEventListener(
       "mouseenter",
-      () => (newDiv.style.backgroundColor = "blue")
+      () => (newDiv.style.backgroundColor = "black")
     );
     board.appendChild(newDiv);
   }
-})();
+}
